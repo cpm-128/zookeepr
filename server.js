a6e3faae54f9ba2a3c112dac2bbc261d987eb324
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-06-20 23:02:38 -0400

    add req.param and 404 error

--- server.js
+++ server.js
@@ -51,6 +51,11 @@ function filterByQuery(query , animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 // get data
 app.get('/api/animals', (req,res) => {
     let results = animals;
@@ -58,6 +63,16 @@ app.get('/api/animals', (req,res) => {
         results = filterByQuery(req.query , results);
     }
     res.json(results);
+});
+
+// a param route MUST come after the get route above
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
