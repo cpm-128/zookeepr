8f7250400bc9ec1adbdd72af0bf7c07285112025
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-06-22 16:55:15 -0400

    updated pathing in htmlRoutes index

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -3,17 +3,17 @@ const router = require('express').Router();
 
 // set up index.html ('/') to be served from the server --> only job is to respond with the HTML page in the browser
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
 // set up animals.html to respond when requested in the client browser
 router.get('/animals', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/animals.html'));
+    res.sendFile(path.join(__dirname, '../../public/animals.html'));
 });
 
 // set up zookeepers.html to respond when requested in the client
 router.get('/zookeepers', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+    res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
 });
 
 // add a wildcard to respond when a user sends a request that does not exist
