92ccedcad11488c9ab8135cafd4105a0e4772214
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-06-22 17:17:57 -0400

    search and display zookeepers

--- public/assets/js/zookeeper.js
+++ public/assets/js/zookeeper.js
@@ -1,9 +1,10 @@
 const $displayArea = document.querySelector('#display-area');
+const $zookeeperForm = document.querySelector('#zookeeper-form');
 
 const printResults = resultArr => {
   console.log(resultArr);
 
-  const animalHTML = resultArr.map(({ id, name, age, favoriteAnimal }) => {
+  const zookeeperHTML = resultArr.map(({ id, name, age, favoriteAnimal }) => {
     return `
   <div class="col-12 col-md-5 mb-3">
     <div class="card p-3" data-id=${id}>
@@ -17,7 +18,7 @@ const printResults = resultArr => {
     `;
   });
 
-  $displayArea.innerHTML = animalHTML.join('');
+  $displayArea.innerHTML = zookeeperHTML.join('');
 };
 
 const getZookeepers = (formData = {}) => {
@@ -39,5 +40,20 @@ const getZookeepers = (formData = {}) => {
       printResults(zookeeperArr);
     });
 };
+
+const handleGetZookeepersSubmit = event => {
+  event.preventDefault();
+  const nameHTML = $zookeeperForm.querySelector('[name="name"]');
+  const name = nameHTML.value;
+
+  const ageHTML = $zookeeperForm.querySelector('[name="age"]');
+  const age = ageHTML.value
+
+  const zookeeperObject = { name , age };
+
+  getZookeepers(zookeeperObject);
+};
+
+$zookeeperForm.addEventListener('submit' , handleGetZookeepersSubmit);
 
 getZookeepers();
